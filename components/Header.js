dcc454d8b56c3877450f538896de8028436d1de8
N4oto-T <h13.only57@gmail.com>
2024-01-28 15:16:37 +0900

    video-6 Link component

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import classes from "./Header.module.css";
+export function Header() {
+  return (
+    <header className={classes.header}>
+      {/* aタグ使ってリンク作成した場合、ページのリロードが走ってしまい描画が遅くなる */}
+      <Link href="/">Index</Link>
+      <Link href="/about">About</Link>
+    </header>
+  );
+}
--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
 import { Main } from "../components/Main";
 import styles from "../styles/Home.module.css";
-
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -10,6 +10,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
 
       <Main page="about" />
 
