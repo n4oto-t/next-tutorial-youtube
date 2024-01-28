d63cb1d1ab5c76b793a24a513dac9c34410e4c5b
N4oto-T <h13.only57@gmail.com>
2024-01-28 14:46:15 +0900

    ファイル名先頭の余分なスペース削除

---
+++ components/Main.js
@@ -0,0 +1,16 @@
+import { Headline } from "./Headline";
+import { Links } from "./Links";
+import classes from "./Main.module.css";
+
+export function Main(props) {
+  return (
+    <div className={classes.container}>
+      <main className={classes.main}>
+        <Headline page={props.page} />
+        <code className={classes.code}>pages/{props.page}.js</code>
+
+        <Links />
+      </main>
+    </div>
+  );
+}
