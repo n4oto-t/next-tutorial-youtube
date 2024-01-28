1e3e60813db0837a4576cb8261aff061f9bab4f8
N4oto-T <h13.only57@gmail.com>
2024-01-28 10:26:44 +0900

    video2-Footerをコンポーネント化

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { Footer } from "../components/Footer";
 import styles from "../styles/Home.module.css";
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -11,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Index page <a href="https://nextjs.org">Next.js!</a>
+          About page <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
@@ -50,7 +51,7 @@ export default function Home() {
         </div>
       </main>
 
-      <Footer />
+      <Footer></Footer>
     </div>
   );
 }
