73b6441d4f6ac43ea506355631da9bb663b0974f
N4oto-T <h13.only57@gmail.com>
2024-01-31 18:51:53 +0900

    video-11 useState

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,11 @@
 import Head from "next/head";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 import styles from "src/styles/Home.module.css";
 export default function Home() {
-  // const foo = 1;
+  const [foo, setFoo] = useState(1);
   useEffect(() => {
     // マウント時の動作を定義
     document.body.style.backgroundColor = "lightblue";
@@ -15,11 +15,9 @@ export default function Home() {
     };
   }, []);
 
-  // const handleClick = useCallback((e) => {
-  //   console.log(e.target.href);
-  //   e.preventDefault();
-  //   alert(foo);
-  // }, []);
+  const handleClick = (e) => {
+    setFoo((foo) => foo + 1);
+  };
 
   return (
     <div className={styles.container}>
@@ -27,9 +25,8 @@ export default function Home() {
         <title>Create Next App</title>
       </Head>
       <Header />
-      {/* <a href="/about" onClick={handleClick}>
-        a tag
-      </a> */}
+      <h1>{foo}</h1>
+      <button onClick={handleClick}>button</button>
       <Main page="index" />
 
       <Footer />
