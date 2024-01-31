819ea0df6075dcb610a2e2e912dcd0eeec437bdf
N4oto-T <h13.only57@gmail.com>
2024-01-31 18:36:38 +0900

    video-10 mount , unmount時の動作追加

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,8 +1,15 @@
 import { Headline } from "src/components/Headline";
 import { Links } from "src/components/Links";
 import classes from "src/components/Main/Main.module.css";
-
 export function Main(props) {
+  // useEffect(() => {
+  //   // mountの動作を定義
+  //   document.body.style.backgroundColor = "lightblue";
+  //   // unmount時の動作を定義
+  //   return () => {
+  //     document.body.style.backgroundColor = "";
+  //   };
+  // }, []);
   return (
     <div className={classes.container}>
       <main className={classes.main}>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,25 @@
 import Head from "next/head";
-import { useCallback } from "react";
+import { useEffect } from "react";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 import styles from "src/styles/Home.module.css";
-
 export default function Home() {
-  const foo = 1;
-
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
+  // const foo = 1;
+  useEffect(() => {
+    // マウント時の動作を定義
+    document.body.style.backgroundColor = "lightblue";
+    // アンマウント時の動作を定義
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
   }, []);
+
+  // const handleClick = useCallback((e) => {
+  //   console.log(e.target.href);
+  //   e.preventDefault();
+  //   alert(foo);
+  // }, []);
 
   return (
     <div className={styles.container}>
@@ -20,9 +27,9 @@ export default function Home() {
         <title>Create Next App</title>
       </Head>
       <Header />
-      <a href="/about" onClick={handleClick}>
+      {/* <a href="/about" onClick={handleClick}>
         a tag
-      </a>
+      </a> */}
       <Main page="index" />
 
       <Footer />
