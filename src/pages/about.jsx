ccd94e43966cc8046fce744eaa7e3132fae8bcf2
N4oto-T <h13.only57@gmail.com>
2024-02-01 21:06:33 +0900

    video-17 useMemo

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -5,7 +5,7 @@ import { Main } from "src/components/Main";
 import styles from "src/styles/Home.module.css";
 
 export default function About({
-  count,
+  doubleCount,
   isShow,
   handleClick,
   handleDisplay,
@@ -20,7 +20,7 @@ export default function About({
         <title>Create Next App</title>
       </Head>
       <Header />
-      {isShow ? <h1>{count}</h1> : null}
+      {isShow ? <h1>{doubleCount}</h1> : null}
       <button onClick={handleClick}>button</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
       <input type="text" value={text} onChange={handleChange} />
