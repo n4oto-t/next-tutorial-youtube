42af5336a0adc889973790d0170c2506500000c6
N4oto-T <h13.only57@gmail.com>
2024-02-01 19:08:49 +0900

    video16 コンポーネント間のリフトアップ

--- src/components/Headline/index.jsx
+++ src/components/Headline/index.jsx
@@ -1,13 +1,12 @@
-import classes from 'src/components/Headline/Headline.module.css';
+import classes from "src/components/Headline/Headline.module.css";
 
 export function Headline(props) {
   return (
     <div>
       <h1 className={classes.title}>{props.page} page</h1>
 
-      <p className={classes.description}>
-        Get started by editing {props.children}
-      </p>
+      <p className={classes.description}>item count: {props.children}</p>
+      <button onClick={props.handleReduce}>減らす</button>
     </div>
   );
 }
